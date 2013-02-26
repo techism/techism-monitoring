1da7d2129756101d19a18f2768fd0546b85888f8
Christine Koppelt <ch.ko123@googlemail.com>
2013-02-26 01:41:06 +0100

    change sort order

--- techism/persistence.go
+++ techism/persistence.go
@@ -35,8 +35,8 @@ func get_sites_with_status_error_or_ok (c appengine.Context)([]Site, []*datastor
 }
 
 func get_all_sites (c appengine.Context)([]Site, []*datastore.Key, error){
-    //TODO
-	q := datastore.NewQuery("Site").Order("-Title").Limit(500)
+    //TODO get all
+	q := datastore.NewQuery("Site").Order("Title").Limit(500)
     sites := make([]Site, 0, 500)
     keys, err := q.GetAll(c, &sites);
     return sites, keys, err
